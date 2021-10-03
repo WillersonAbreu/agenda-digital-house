8d7b4fbb53a4c90c7a88a6dc14e21a733ed262f1
Willerson Abreu <willersonabreu@hotmail.com>
2021-10-03 15:05:58 -0300

    fix: Removendo diretório das imagens do restart devtools

--- src/main/java/br/com/santander/agenda/config/WebSecurityConfig.java
+++ src/main/java/br/com/santander/agenda/config/WebSecurityConfig.java
@@ -13,11 +13,9 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebSecurity
